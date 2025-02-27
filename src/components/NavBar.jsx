7acc6f179908b7a9c862c2aa5b58ec02311b30cb
jitendra Suthar <jitsuthar2003@gmail.com>
2025-02-28 02:13:34 +0530

    Added: basic navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+const NavBar = () => {
+  return (
+    <>
+        <section className="py-4">
+            <div className="container w-full max-w-full mx-4">
+                <div className="grid grid-cols-2 border border-white/16 rounded-full py-2 px-4">
+                    <div>logo</div>
+                    <div>icon</div>
+                </div>
+            </div>
+        </section>
+    
+    </>
+  );
+}
+export default NavBar;
