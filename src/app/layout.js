0512a4b5c6ca7e2280b384b8976cb2ffd96a9676
jitendra Suthar <jitsuthar2003@gmail.com>
2025-03-28 15:48:54 +0530

    Added: Analytics component of vercel

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -32,6 +33,7 @@ export default function RootLayout({ children }) {
         
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem={true}>
           {children}
+          <Analytics />
         </ThemeProvider>
 
       </body>
